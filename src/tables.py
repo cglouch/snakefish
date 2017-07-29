834546a26431bb525ace0dff430bcf72048ce7be
Cameron Glouchevitch <cameron.glouchevitch@gmail.com>
2017-07-29 17:07:41 -0400

    Added mask tables

--- src/tables.py
+++ src/tables.py
@@ -16,8 +16,42 @@ FILES = np.array(
             [np.uint64(0x0101010101010101) << np.uint8(i) for i in range(8)],
             dtype=np.uint64)
 
+RANK_MASKS = np.fromiter(
+        (RANKS[i//8] for i in range(64)),
+        dtype=np.uint64,
+        count=64)
+
+FILE_MASKS = np.fromiter(
+        (FILES[i%8] for i in range(64)),
+        dtype=np.uint64,
+        count=64)
+
 A1H8_DIAG = np.uint64(0x8040201008040201)
 H1A8_ANTIDIAG = np.uint64(0x0102040810204080)
+
+def compute_diag_mask(i):
+    diag = 8*(i & 7) - (i & 56)
+    north = -diag & (diag >> 31)
+    south = diag & (-diag >> 31)
+    return (A1H8_DIAG >> np.uint8(south)) << np.uint8(north)
+
+DIAG_MASKS = np.fromiter(
+        (compute_diag_mask(i) for i in range(64)),
+        dtype=np.uint64,
+        count=64)
+
+def compute_antidiag_mask(i):
+    diag = 56 - 8*(i & 7) - (i & 56)
+    north = -diag & (diag >> 31)
+    south = diag & (-diag >> 31)
+    return (H1A8_ANTIDIAG >> np.uint8(south)) << np.uint8(north)
+
+ANTIDIAG_MASKS = np.fromiter(
+        (compute_antidiag_mask(i) for i in range(64)),
+        dtype=np.uint64,
+        count=64)
+
+
 
 # Bitboards for move generation
 
@@ -121,12 +155,11 @@ PAWN_ATTACKS.shape = (2,64)
 
 # FIRST RANK MOVES
 # Array is indexed by file of square and occupancy of line
-# Rows of a given entry are duplicated and contain possible sliding piece movements for given square, occupancy of first rank
 
 def compute_first_rank_moves(i, occ):
     # i is square index from 0 to 8
     # occ is 8-bit number that represents occupancy of the rank 
-    # Returns bitboard with copies of resulting moveset in each row
+    # Returns first rank moves (as uint64 bitboard)
 
     occ = np.uint64(occ)
 
@@ -162,12 +195,16 @@ def compute_first_rank_moves(i, occ):
     result = (left_blocker | right_mask(left_blocker)) & (right_blocker | left_mask(right_blocker))
     result &= RANKS[Rank.ONE] # Isolate first rank
 
+    #TODO: maybe change this to 8-bit version since we don't need the row duplication
+
+    """
     # At this point first rank of result holds moveset for given square and occupancy
     # We finish off by copying this pattern to the other ranks
 
     result |= result << np.uint8(8)
     result |= result << np.uint8(16)
     result |= result << np.uint8(32)
+    """
 
     return result
 
