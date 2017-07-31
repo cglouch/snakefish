1d7b31f5a43044a1398d8b326d4fb9dcae4ac31c
Cameron Glouchevitch <cameron.glouchevitch@gmail.com>
2017-07-31 18:14:35 -0400

    Added from_str constructor

--- src/square.py
+++ src/square.py
@@ -15,6 +15,12 @@ class Square(object):
     def from_position(cls, r, f):
         return cls((r.value << np.uint8(3)) | f.value) # 8*rank + file
 
+    @classmethod
+    def from_str(cls, st):
+        f = np.uint8(ord(st[0]) - ord('A'))
+        r = np.uint8(int(st[1]) - 1)
+        return cls((r << np.uint8(3)) | f) # 8*rank + file
+
     def to_bitboard(self):
         return np.uint64(1) << self.index
 
